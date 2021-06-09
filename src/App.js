178dfab37a38420285644dd75535f4b9bd01edec
jerryyuan10 <71941049+jerryyuan10@users.noreply.github.com>
2021-06-08 20:15:40 -0500

    Created and linked articles list & Make articles list modular & Created a 404 page
    - Displayed article titles in the articles list page (use map)
    - Used {Link} to link to each article
    - Added text sample for each article (no more than 150 characters)
    
    - Create reusable articles list component:
      -- Renamed the ArticleList.js to ArticleListPage.js
     -- Created components directory and ArticlesList.js
     -- Added articles as prop in ArticlesList.js
     -- Added reusable other articles underneath each article using ArticleList.js (module)
    
    - Created NotFoundPage.js under page file
    - Created 404 page Route without path (always match by default)
    - Added Switch
    - Replaced <h1>Article does not exist!</h1> to <NotFoundPage />

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
+  Switch,
 } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
-import ArticlesList from './pages/ArticlesList';
+import ArticlesListPage from './pages/ArticlesListPage';
 import ArticlePage from './pages/ArticlePage';
 import NavBar from './NavBar';
 import './App.css';
+import NotFoundPage from './pages/NotFoundPage';
 
 class App extends Component {
   render() {
@@ -17,10 +19,13 @@ class App extends Component {
       <div className="App">
         <NavBar />
         <div id="page-body">
+          <Switch>
         <Route path="/" component={HomePage} exact />
         <Route path="/about" component={AboutPage} />
-        <Route path="/articles-list" component={ArticlesList} />
+        <Route path="/articles-list" component={ArticlesListPage} />
         <Route path="/article/:name" component={ArticlePage} />
+        <Route component={NotFoundPage} />
+          </Switch>
         </div>
       </div>
       </Router>
